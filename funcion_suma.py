b4041188d35b2dd4412b0be6d927fc8377ea9601
Juan Solis <134623680+zerozuk@users.noreply.github.com>
2026-04-04 23:48:37 -0500

    Delete funcion_suma.py

--- funcion_suma.py
+++
@@ -1,9 +0,0 @@
-def suma(a, b):
-    return a + b
-
-
-print("===Función suma===")
-a = int(input("Ingrese el primer numero para la suma: "))
-b = int(input("Ingrese el segundo número: "))
-result = suma(a, b)
-print("El resultado de la suma es:", result)
